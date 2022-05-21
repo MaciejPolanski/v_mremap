f9e63d6839c69169e21e8fd121d1df3256c5f44f
Maciej Polanski <mega@shodan-mint>
2022-05-21 02:14:24 +0200

    Example rephrased

--- mremap_test.c
+++ mremap_test.c
@@ -17,22 +17,32 @@ void err(void* e, int code)
 
 int main()
 {
+    // This test attaches new mapped memory to 'a' chunk.
+    // However, it fails at second time for unclear reason.
+    // First, let's create initial mmapped region 'a'
     void* a = mmap(NULL, size, PROT_EXEC | PROT_READ | PROT_WRITE, 
             MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, /*not a file mapping*/-1, 0);
     err(a, 1);
-    // Grow 'a' to double size, also poof that grow is working
-    a = mremap(a, size, 2 * size, MREMAP_MAYMOVE);
-    err(a, 2);
 
+    // Creation of 'b' and attaching it at the end of 'a'
+    // 'a' need to be enlarged first, relocation is possible
     void* b = mmap(NULL, size, PROT_EXEC | PROT_READ | PROT_WRITE, 
             MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, /*not a file mapping*/-1, 0);
-    err(b, 3);    
-    // Let's overwrite 'b' as second half of 'a'
-    b = mremap(b, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, a + size);
+    err(b, 2);    
+    a = mremap(a, size, 2 * size, MREMAP_MAYMOVE);                      // Grow 'a'
+    err(a, 3);
+    b = mremap(b, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, a + size); // Attach 'b' at the end
     err(b, 4);
-    // And grow 'a' again. It fails, but only with overwrite above^
-    a = mremap(a, 2 * size, 3 * size, MREMAP_MAYMOVE);
-    err(a, 5);
+
+    // Creation of 'c' and attaching it at the end of 'a'
+    // 'a' need to be enlarged first, relocation is possible
+    void* c = mmap(NULL, size, PROT_EXEC | PROT_READ | PROT_WRITE, 
+            MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE, /*not a file mapping*/-1, 0);
+    err(c, 5);    
+    a = mremap(a, 2 * size, 3 * size, MREMAP_MAYMOVE);                   // <- this fails, code 6
+    err(a, 6);
+    c = mremap(c, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, a + 2 * size); 
+    err(b, 7);
 
     return 0;
 }
